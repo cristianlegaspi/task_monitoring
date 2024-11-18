016828c4f0f5ca6991d4f8c6fd9fe885ea72ab73
cristianlegaspi <cglegaspi@tesda.gov.ph>
2024-11-18 18:29:57 +0800

    Latest Commit

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskBoardController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::post('/update-task-status', function () {
-    // Get the task ID and new status from the request
-    $taskId = request('id');
-    $status = request('status');
-
-    // Find the task and update its status
-    $task = Task::findOrFail($taskId);
-    $task->status = $status;
-    $task->save();
-
-    // Return a JSON response with the updated task data
-    return response()->json($task);
+    return redirect('/admin/login');
 });
